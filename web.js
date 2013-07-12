bd649ab99200a7b13e0ccd532b2905ac5cabf927
hirashrestha <hirashrestha@gmail.com>
2013-07-12 03:37:45 +0000

    Read index.html

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
+var file = fs.readFileSync("index.html", "utf8");
+var buffer = new Buffer(file, "utf8");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  response.send(buffer.toString("utf8"));
+  });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
